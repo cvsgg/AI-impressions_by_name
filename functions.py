d7fb7e8ffdfa142a7c69268db3f147cd1b55ad64
Caio Salim <110683021+cvsgg@users.noreply.github.com>
2023-07-04 23:21:04 -0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,47 @@
+import spacy
+import requests
+from bs4 import BeautifulSoup
+import openai
+
+def get_info_website(url, words_limit=None):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    text = soup.get_text()
+    if words_limit is not None:
+        palavras = text.split()
+        text = ' '.join(palavras[:words_limit])
+    return text
+
+def extract_names(text):
+    nlp = spacy.load('en_core_web_sm')
+    doc = nlp(text)
+    names = []
+    for i in doc.ents:
+        if i.label_ == 'PERSON':
+            names.append(i.text)
+
+    return names
+
+def get_comments(text, names, api_key):
+    openai.api_key = api_key
+    comments_per_name = {name: [] for name in names}
+
+    for name in names:
+        prompt = f"Get the short comment in {text} talking about the {name}:"
+
+        response = openai.Completion.create(
+            engine='davinci', 
+            prompt=prompt,
+            max_tokens=100,
+            temperature=0.3,
+            top_p=1.0,
+            n=5,
+            stop=None
+        )
+
+        if response.choices:
+            for name in comments_per_name:
+                comments = response.choices[0].text.strip()
+                comments_per_name[name].append(comments)
+
+    return comments_per_name
---
+++ main.py
@@ -0,0 +1,15 @@
+import functions
+import parameters
+
+url = parameters.url
+api_key = parameters.api_key
+
+text_ex = functions.get_info_website(url, 200)
+list_names = functions.extract_names(text_ex)
+comments_per_name = functions.get_comments(text_ex, list_names, api_key)
+
+get_dict = {
+    f"{name}":f"{comments}"
+    for name, comments in comments_per_name.items()
+}
+print(get_dict)
