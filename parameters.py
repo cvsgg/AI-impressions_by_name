4aabf0bf217149f4e0b74f6e09bc764a8b0d6ec9
Caio Salim <110683021+cvsgg@users.noreply.github.com>
2023-07-09 12:11:22 -0300

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -1,2 +1,6 @@
 api_key = 'api_key'
 url = "https://www.nba.com/news/"
+tryout_text = """John is a talented musician. He plays the guitar and piano. His compositions are truly remarkable. 
+    Lisa is a brilliant scientist. She has made significant contributions to the field of physics. 
+    Michael is an exceptional athlete. He has won several championships in swimming and running.
+    Sarah is a renowned author. Her novels have captivated readers worldwide."""
