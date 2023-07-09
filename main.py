a760378dc378bee012369ef4ed650bb84c54411a
Caio Salim <110683021+cvsgg@users.noreply.github.com>
2023-07-09 13:06:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,8 @@ url = parameters.url
 api_key = parameters.api_key
 
 text_ex = functions.get_info_website(url, 200)
-list_names = functions.extract_names(text_ex)
+list_names = functions.extract_names(parameters.tryout_text) ### If you don't need to extract the text from some url, please skip function get_info_website and put your text directly here
 print(list_names)
-comments_per_name = functions.get_comments(text_ex, list_names, api_key)
+comments_per_name = functions.get_comments(parameters.tryout_text, list_names, api_key)
 
-get_dict = {
-    f"{name}":f"{comments}"
-    for name, comments in comments_per_name.items()
-}
-print(get_dict)
+print(comments_per_name)
