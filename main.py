5a8a81c1f3b8c11f49b15a32e350d3f84722aa23
Caio Salim <110683021+cvsgg@users.noreply.github.com>
2023-07-09 12:52:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ api_key = parameters.api_key
 
 text_ex = functions.get_info_website(url, 200)
 list_names = functions.extract_names(text_ex)
+print(list_names)
 comments_per_name = functions.get_comments(text_ex, list_names, api_key)
 
 get_dict = {
